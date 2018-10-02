3cb6d7bf488c064db5c0aa86e12f6dfe8e5a909a
dferrer8 <david.dfg@gmail.com>
2018-10-02 16:19:38 +0200

    Nueva pagina contactar

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hola Mundos')
+    res.send('Hola Mundo')
+})
+app.get('/contactar', (req, res) => {
+    res.send('Página para contactar')
 })
 app.listen(3000, ()=> {console.log('Servidor levantado')} // puerto, función callback
 
